0188e4ee5c42e759492d58ebb4a572401971e603
HAJIME Fukuna <f97one@gmail.com>
2013-03-17 22:40:08 +0900

    プリファレンス用の1Wから7Wまでの言語リソースを追加した。

--- gen/net/formula97/android/pocket_score_card/R.java
+++ gen/net/formula97/android/pocket_score_card/R.java
@@ -32,9 +32,15 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f060000;
+        public static final int clubname_1wood=0x7f06000e;
+        public static final int clubname_3wood=0x7f06000f;
+        public static final int clubname_4wood=0x7f060010;
+        public static final int clubname_5wood=0x7f060011;
+        public static final int clubname_6wood=0x7f060012;
+        public static final int clubname_7wood=0x7f060013;
         public static final int course_info=0x7f060005;
         public static final int hello_world=0x7f060001;
-        public static final int menu_settings=0x7f06000e;
+        public static final int menu_settings=0x7f060014;
         public static final int play_cont=0x7f060004;
         public static final int player_info=0x7f060002;
         public static final int pref_basic_hdcp=0x7f060009;
