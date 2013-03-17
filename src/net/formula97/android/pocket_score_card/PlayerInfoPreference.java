745766600668cdb86adcf6ef53295f49b78f981f
HAJIME Fukuna <f97one@gmail.com>
2013-03-17 12:41:14 +0900

    プリファレンス表示用のClassを追加。

---
+++ src/net/formula97/android/pocket_score_card/PlayerInfoPreference.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package net.formula97.android.pocket_score_card;
+
+import android.preference.PreferenceActivity;
+
+/**
+ * @author kazutoshi
+ *
+ */
+public class PlayerInfoPreference extends PreferenceActivity {
+
+	/**
+	 * 
+	 */
+	public PlayerInfoPreference() {
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
+
+}
