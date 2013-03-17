f2ade7cefa7245ecdc8b8ef41c1e78369768ab81
HAJIME Fukuna <f97one@gmail.com>
2013-03-17 12:11:32 +0900

    メインタイトルに、プレイ／コンティニューを表すボタンを追加。

--- gen/net/formula97/android/pocket_score_card/R.java
+++ gen/net/formula97/android/pocket_score_card/R.java
@@ -20,7 +20,8 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int menu_settings=0x7f080000;
+        public static final int btn_playcont=0x7f080000;
+        public static final int menu_settings=0x7f080001;
     }
     public static final class layout {
         public static final int activity_main_title=0x7f030000;
@@ -32,6 +33,7 @@ public final class R {
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050001;
         public static final int menu_settings=0x7f050002;
+        public static final int play_cont=0x7f050004;
         public static final int title_activity_main_title=0x7f050003;
     }
     public static final class style {
