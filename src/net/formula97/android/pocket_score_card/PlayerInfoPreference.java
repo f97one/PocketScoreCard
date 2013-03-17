108f294ca72a8b3d6e155d2ab4e6cecfee39d995
HAJIME Fukuna <f97one@gmail.com>
2013-03-17 21:05:23 +0900

    PreferenceActivity#addPreferenceFromResource()を追加。
    ただし、Deprecatedと出るので、何らかの対策は必要と思う。

--- src/net/formula97/android/pocket_score_card/PlayerInfoPreference.java
+++ src/net/formula97/android/pocket_score_card/PlayerInfoPreference.java
@@ -1,8 +1,9 @@
 /**
- * 
+ *
  */
 package net.formula97.android.pocket_score_card;
 
+import android.os.Bundle;
 import android.preference.PreferenceActivity;
 
 /**
@@ -12,10 +13,20 @@ import android.preference.PreferenceActivity;
 public class PlayerInfoPreference extends PreferenceActivity {
 
 	/**
-	 * 
+	 *
 	 */
 	public PlayerInfoPreference() {
 		// TODO 自動生成されたコンストラクター・スタブ
+	}
+
+	/* (非 Javadoc)
+	 * @see android.preference.PreferenceActivity#onCreate(android.os.Bundle)
+	 */
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO 自動生成されたメソッド・スタブ
+		super.onCreate(savedInstanceState);
+		addPreferencesFromResource(R.xml.player_info);
 	}
 
 }
