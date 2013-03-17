4e22997b90189495755742395057c16bb2792619
HAJIME Fukuna <f97one@gmail.com>
2013-03-17 12:36:34 +0900

    プレイヤー情報編集用のPreferenceを作成。

--- gen/net/formula97/android/pocket_score_card/R.java
+++ gen/net/formula97/android/pocket_score_card/R.java
@@ -11,32 +11,46 @@ public final class R {
     public static final class attr {
     }
     public static final class dimen {
-        public static final int padding_large=0x7f040002;
-        public static final int padding_medium=0x7f040001;
-        public static final int padding_small=0x7f040000;
+        public static final int padding_large=0x7f050002;
+        public static final int padding_medium=0x7f050001;
+        public static final int padding_small=0x7f050000;
     }
     public static final class drawable {
         public static final int ic_action_search=0x7f020000;
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int btn_playcont=0x7f080000;
-        public static final int menu_settings=0x7f080001;
+        public static final int btn_playcont=0x7f090000;
+        public static final int course_info=0x7f090002;
+        public static final int player_info=0x7f090001;
     }
     public static final class layout {
         public static final int activity_main_title=0x7f030000;
     }
     public static final class menu {
-        public static final int activity_main_title=0x7f070000;
+        public static final int activity_main_title=0x7f080000;
     }
     public static final class string {
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050001;
-        public static final int menu_settings=0x7f050002;
-        public static final int play_cont=0x7f050004;
-        public static final int title_activity_main_title=0x7f050003;
+        public static final int app_name=0x7f060000;
+        public static final int course_info=0x7f060005;
+        public static final int hello_world=0x7f060001;
+        public static final int menu_settings=0x7f06000e;
+        public static final int play_cont=0x7f060004;
+        public static final int player_info=0x7f060002;
+        public static final int pref_basic_hdcp=0x7f060009;
+        public static final int pref_basic_info=0x7f060006;
+        public static final int pref_basic_player_name=0x7f060008;
+        public static final int pref_club_irons=0x7f06000b;
+        public static final int pref_club_putter=0x7f06000d;
+        public static final int pref_club_wedges=0x7f06000c;
+        public static final int pref_club_woods=0x7f06000a;
+        public static final int pref_using_clubs=0x7f060007;
+        public static final int title_activity_main_title=0x7f060003;
     }
     public static final class style {
-        public static final int AppTheme=0x7f060000;
+        public static final int AppTheme=0x7f070000;
+    }
+    public static final class xml {
+        public static final int player_info=0x7f040000;
     }
 }
