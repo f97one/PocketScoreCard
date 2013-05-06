a4d7708fab6393a9523e8685f594c6ab5d77a0fc
HAJIME Fukuna <f97one@gmail.com>
2013-05-06 20:29:54 +0900

    プロジェクトで扱う定数を格納するクラスを作成。

---
+++ src/net/formula97/android/pocket_score_card/PocketScoreCardConstants.java
@@ -0,0 +1,15 @@
+/**
+ * プロジェクトで扱う定数を格納する。
+ */
+package net.formula97.android.pocket_score_card;
+
+/**
+ * プロジェクトで扱う定数を格納する。
+ * @author HAJIME Fukuna
+ *
+ */
+public final class PocketScoreCardConstants {
+
+	// Windows Live Connectで使用するクライアントID
+	public static final String LIVE_APP_CLIENT_ID = "000000004C0EC756";
+}
