b1454113d7c137a6ea7f4f95239b5a54fef6a406
HAJIME Fukuna <f97one@gmail.com>
2013-05-06 20:38:09 +0900

    今後の拡張を考慮して、クラス設計を階層にしてみた。

--- src/net/formula97/android/pocket_score_card/AppConstants.java
+++ src/net/formula97/android/pocket_score_card/AppConstants.java
@@ -10,6 +10,12 @@ package net.formula97.android.pocket_score_card;
  */
 public final class AppConstants {
 
-	// Windows Live Connectで使用するクライアントID
-	public static final String LIVE_APP_CLIENT_ID = "000000004C0EC756";
+	// Windows Live Connectで使用するパラメータ類
+	public static final class Live {
+		// クライアントID（Windowsストアアプリで言うところのパッケージID）
+		public static final String CLIENT_ID = "000000004C0EC756";
+		// クライアントシークレット
+		public static final String CLIENT_SECRET = "4z9cLR-Y3YPKv0yPedcKbgErF5a9SmDs";
+	}
+
 }
