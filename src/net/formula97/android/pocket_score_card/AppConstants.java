2b189bfdd8b1d8d8d5e979f5b77721ceb2198139
HAJIME Fukuna <f97one@gmail.com>
2013-05-06 20:33:02 +0900

    クラス名が無駄に長くなったので、クラス名を短縮した。

--- src/net/formula97/android/pocket_score_card/AppConstants.java
+++ src/net/formula97/android/pocket_score_card/AppConstants.java
@@ -8,7 +8,7 @@ package net.formula97.android.pocket_score_card;
  * @author HAJIME Fukuna
  *
  */
-public final class PocketScoreCardConstants {
+public final class AppConstants {
 
 	// Windows Live Connectで使用するクライアントID
 	public static final String LIVE_APP_CLIENT_ID = "000000004C0EC756";
