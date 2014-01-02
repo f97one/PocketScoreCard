ac4a14ef3a15f6085c2d53c639d7150d93014033
HAJIME Fukuna <f97one@gmail.com>
2014-01-02 16:37:21 +0900

    loadPlayerDataFromPreference()の本体を仮実装。

--- src/net/formula97/android/pocket_score_card/PlayerInfoPreference.java
+++ src/net/formula97/android/pocket_score_card/PlayerInfoPreference.java
@@ -4,13 +4,45 @@
 package net.formula97.android.pocket_score_card;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.ToggleButton;
 
 /**
  * @author kazutoshi
  *
  */
 public class PlayerInfoPreference extends Activity {
+
+	SharedPreferences sp;
+	
+	EditText editText_ownerName;
+	EditText editText_ownerHDCP;
+	Spinner spinner_clubSettingType;
+	ToggleButton toggleButton_1w;
+	ToggleButton toggleButton_3w;
+	ToggleButton toggleButton_4w;
+	ToggleButton toggleButton_5w;
+	ToggleButton toggleButton_6w;
+	ToggleButton toggleButton_7w;
+	ToggleButton toggleButton_ut3;
+	ToggleButton toggleButton_ut5;
+	ToggleButton toggleButton_ut7;
+	ToggleButton toggleButton_ut9;
+	ToggleButton toggleButton_3i;
+	ToggleButton toggleButton_4i;
+	ToggleButton toggleButton_5i;
+	ToggleButton toggleButton_6i;
+	ToggleButton toggleButton_7i;
+	ToggleButton toggleButton_8i;
+	ToggleButton toggleButton_9i;
+	ToggleButton toggleButton_pw;
+	ToggleButton toggleButton_aw;
+	ToggleButton toggleButton_lw;
+	ToggleButton toggleButton_sw;
+	ToggleButton toggleButton_pt;
 
 	/**
 	 *
@@ -27,6 +59,36 @@ public class PlayerInfoPreference extends Activity {
 		// TODO 自動生成されたメソッド・スタブ
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_owner_infomation);
+		
+		// Preferenceを取得
+		sp = getSharedPreferences(ProjConstants.Prefs.PREF_NAME, MODE_PRIVATE);
+		
+		// ViewIdを取得
+		editText_ownerName = (EditText)findViewById(R.id.editText_ownerName);
+		editText_ownerHDCP = (EditText)findViewById(R.id.editText_ownerHDCP);
+		spinner_clubSettingType = (Spinner)findViewById(R.id.spinner_clubSettingType);
+		toggleButton_1w = (ToggleButton)findViewById(R.id.toggleButton_1w);
+		toggleButton_3w = (ToggleButton)findViewById(R.id.toggleButton_3w);
+		toggleButton_4w = (ToggleButton)findViewById(R.id.toggleButton_4w);
+		toggleButton_5w = (ToggleButton)findViewById(R.id.toggleButton_5w);
+		toggleButton_6w = (ToggleButton)findViewById(R.id.toggleButton_6w);
+		toggleButton_7w = (ToggleButton)findViewById(R.id.toggleButton_7w);
+		toggleButton_ut3 = (ToggleButton)findViewById(R.id.toggleButton_ut3);
+		toggleButton_ut5 = (ToggleButton)findViewById(R.id.toggleButton_ut5);
+		toggleButton_ut7 = (ToggleButton)findViewById(R.id.toggleButton_ut7);
+		toggleButton_ut9 = (ToggleButton)findViewById(R.id.toggleButton_ut9);
+		toggleButton_3i = (ToggleButton)findViewById(R.id.toggleButton_3i);
+		toggleButton_4i = (ToggleButton)findViewById(R.id.toggleButton_4i);
+		toggleButton_5i = (ToggleButton)findViewById(R.id.toggleButton_5i);
+		toggleButton_6i = (ToggleButton)findViewById(R.id.toggleButton_6i);
+		toggleButton_7i = (ToggleButton)findViewById(R.id.toggleButton_7i);
+		toggleButton_8i = (ToggleButton)findViewById(R.id.toggleButton_8i);
+		toggleButton_9i = (ToggleButton)findViewById(R.id.toggleButton_9i);
+		toggleButton_pw = (ToggleButton)findViewById(R.id.toggleButton_pw);
+		toggleButton_aw = (ToggleButton)findViewById(R.id.toggleButton_aw);
+		toggleButton_lw = (ToggleButton)findViewById(R.id.toggleButton_lw);
+		toggleButton_sw = (ToggleButton)findViewById(R.id.toggleButton_sw);
+		toggleButton_pt = (ToggleButton)findViewById(R.id.toggleButton_pt);
 	}
 
 	/* (non-Javadoc)
@@ -47,11 +109,64 @@ public class PlayerInfoPreference extends Activity {
 		super.onResume();
 	}
 
-	private void loadFromPreference() {
+	/**
+	 * プリファレンスからプレイヤー情報を取得して、画面に反映する。
+	 */
+	private void loadPlayerDataFromPreference() {
+		editText_ownerName.setText(sp.getString(ProjConstants.Prefs.PREF_NAME, ""));
+		editText_ownerHDCP.setText(String.valueOf(sp.getInt(ProjConstants.Prefs.PLAYER_HDCP, 0)));
+		// TODO Spinnerへの値セットを実装する
+		
+		// 所有クラブのセッティングをリストア
+		//   以下のクラブはデフォルトで所有しているものとする。
+		//     1W, 7W, Ut7, 7I, 9I, PW, SW, PT
+		// TODO getClubSettingArray()で取得したboolean[]に変更する。
+		toggleButton_1w.setChecked(sp.getBoolean(ProjConstants.Prefs.CLUB_1W, true));		// 1W
+		toggleButton_3w.setChecked(sp.getBoolean(ProjConstants.Prefs.CLUB_3W, false));		// 3W
+		toggleButton_4w.setChecked(sp.getBoolean(ProjConstants.Prefs.CLUB_4W, false));		// 4W
+		toggleButton_5w.setChecked(sp.getBoolean(ProjConstants.Prefs.CLUB_5W, false));		// 5W
+		toggleButton_6w.setChecked(sp.getBoolean(ProjConstants.Prefs.CLUB_6W, false));		// 6W
+		toggleButton_7w.setChecked(sp.getBoolean(ProjConstants.Prefs.CLUB_7W, true));		// 7W
+		toggleButton_ut3.setChecked(sp.getBoolean(ProjConstants.Prefs.CLUB_UT3, false));	// Ut3
+		toggleButton_ut5.setChecked(sp.getBoolean(ProjConstants.Prefs.CLUB_UT5, false));	// Ut5
+		toggleButton_ut7.setChecked(sp.getBoolean(ProjConstants.Prefs.CLUB_UT7, true));		// Ut7
+		toggleButton_ut9.setChecked(sp.getBoolean(ProjConstants.Prefs.CLUB_UT9, false));	// Ut9
+		toggleButton_3i.setChecked(sp.getBoolean(ProjConstants.Prefs.CLUB_3I, false));		// 3I
+		toggleButton_4i.setChecked(sp.getBoolean(ProjConstants.Prefs.CLUB_4I, false));		// 4I
+		toggleButton_5i.setChecked(sp.getBoolean(ProjConstants.Prefs.CLUB_5W, false));		// 5I
+		toggleButton_6i.setChecked(sp.getBoolean(ProjConstants.Prefs.CLUB_6I, false));		// 6I
+		toggleButton_7i.setChecked(sp.getBoolean(ProjConstants.Prefs.CLUB_7I, true));		// 7I
+		toggleButton_8i.setChecked(sp.getBoolean(ProjConstants.Prefs.CLUB_8I, false));		// 8I
+		toggleButton_9i.setChecked(sp.getBoolean(ProjConstants.Prefs.CLUB_9I, true));		// 9I
+		toggleButton_pw.setChecked(sp.getBoolean(ProjConstants.Prefs.CLUB_PW, true));		// PW
+		toggleButton_aw.setChecked(sp.getBoolean(ProjConstants.Prefs.CLUB_AW, false));		// AW
+		toggleButton_lw.setChecked(sp.getBoolean(ProjConstants.Prefs.CLUB_LW, false));		// LW
+		toggleButton_sw.setChecked(sp.getBoolean(ProjConstants.Prefs.CLUB_SW, true));		// SW
+		toggleButton_pt.setChecked(sp.getBoolean(ProjConstants.Prefs.CLUB_PT, true));		// PT
+	}
+	
+	/**
+	 * プリファレンスへプレイヤー情報を保存する。
+	 */
+	private void savePlayerDataToPrefence() {
 		
 	}
 	
-	private void saveToPrefence() {
+	/**
+	 * クラブセッティングを画面へ反映するために、セッティングタイプに対応するクラブの所有有無をDBから取得する。
+	 * @return ToggleButtonのチェック状態にそのまま反映できる、boolean型の配列
+	 */
+	private boolean[] getClubSettingArray() {
+		// TODO 
+		
+		return null;
+	}
+	
+	/**
+	 * クラブセッティングIDに対応するクラブの所有情報をDBへ格納する。
+	 * @param settingTypeIndex int型、所有情報をひもづけるクラブセッティングID
+	 */
+	private void saveClubSettingArray(int settingTypeIndex) {
 		
 	}
 }
